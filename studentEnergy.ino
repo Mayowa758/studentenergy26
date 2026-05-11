fe6b8e682522d9a8c779f6d6b908121eab74b39c
Mayowa758 <mayowa.oyekan@outlook.com>
2026-05-11 19:58:05 +1000

    base code for EnviroBot

--- studentEnergy.ino
+++ studentEnergy.ino
@@ -1,18 +1,21 @@
-#include <Servo.h>
+#include <ESP32Servo.h>
+#include <BluetoothSerial.h>
+
+BluetoothSerial SerialBT;
 
 // MOTORS
 int motorPinL1 = 2;
 int motorPinL2 = 3;
-int motorEnable = 6;
+int motorLEna = 6;
 
 int motorPinR1 = 4;
 int motorPinR2 = 5;
-int motor2enable = 11;
+int motorREna = 11;
 
-int speed = 100; // the speed of the motors
+int speed = 100; // the base speed of the motors
 
 // SERVOS
-Servo servo1 servo2;
+Servo servo1, servo2;
 int servoPin1 = 7;
 int servoPin2 = 8;
 
@@ -25,6 +28,9 @@ void setup() {
   pinMode(motorPinL2, OUTPUT);
   pinMode(motorPinR2, OUTPUT);
 
+  pinMode(motorLEna, OUTPUT);
+  pinMode(motorREna, OUTPUT);
+
   // Servos Setup
   servo1.attach(servoPin1);
   servo2.attach(servoPin2);
@@ -32,6 +38,13 @@ void setup() {
   // sets base position each time
   servo1.write(baseAngle);
   servo2.write(baseAngle);
+
+  Serial.begin(115200);
+
+  SerialBT.begin("EnviroBot");
+  SerialBT.setPin("1234");
+
+  stopMotors();
 }
 
 // motor helper function
@@ -56,8 +69,8 @@ Move the robot forward.
 Accepts speed (0-255) to set motor speed.
 */
 void moveForward(int speed) {
-  moveMotor(motorPinR1, motorPinR2, motorPinREna, 1, speed);  // Move right motor forward
-  moveMotor(motorPinL1, motorPinL2, motorPinLEna, 1, speed);  // Move left motor forward
+  moveMotor(motorPinR1, motorPinR2, motorREna, 1, speed);  // Move right motor forward
+  moveMotor(motorPinL1, motorPinL2, motorLEna, 1, speed);  // Move left motor forward
 }
 
 /*
@@ -65,8 +78,8 @@ Move the robot backward.
 Accepts speed (0-255) to set motor speed.
 */
 void moveBackward(int speed) {
-  moveMotor(motorPinR1, motorPinR2, motorPinREna, -1, speed);  // Move right motor backward
-  moveMotor(motorPinL1, motorPinL2, motorPinLEna, -1, speed);  // Move left motor backward
+  moveMotor(motorPinR1, motorPinR2, motorREna, -1, speed);  // Move right motor backward
+  moveMotor(motorPinL1, motorPinL2, motorLEna, -1, speed);  // Move left motor backward
 }
 
 /*
@@ -74,8 +87,8 @@ Rotate the robot to the right.
 Right motor moves forward, left motor moves backward.
 */
 void turnRight(int speed) {
-  moveMotor(motorPinR1, motorPinR2, motorPinREna, 1, speed);   // Right motor forward
-  moveMotor(motorPinL1, motorPinL2, motorPinLEna, -1, speed);  // Left motor backward
+  moveMotor(motorPinR1, motorPinR2, motorREna, 1, speed);   // Right motor forward
+  moveMotor(motorPinL1, motorPinL2, motorLEna, -1, speed);  // Left motor backward
 }
 
 /*
@@ -83,59 +96,81 @@ Rotate the robot to the left.
 Right motor moves backward, left motor moves forward.
 */
 void turnLeft(int speed) {
-  moveMotor(motorPinR1, motorPinR2, motorPinREna, -1, speed);  // Right motor backward
-  moveMotor(motorPinL1, motorPinL2, motorPinLEna, 1, speed);   // Left motor forward
+  moveMotor(motorPinR1, motorPinR2, motorREna, -1, speed);  // Right motor backward
+  moveMotor(motorPinL1, motorPinL2, motorLEna, 1, speed);   // Left motor forward
 }
 
 /*
 Stop both motors.
 */
 void stopMotors() {
-  moveMotor(motorPinR1, motorPinR2, motorPinREna, 0, 0);  // Stop right motor (set direction to stop and speed to 0)
-  moveMotor(motorPinL1, motorPinL2, motorPinLEna, 0, 0);  // Stop left motor (set direction to stop and speed to 0)
+  moveMotor(motorPinR1, motorPinR2, motorREna, 0, 0);  // Stop right motor (set direction to stop and speed to 0)
+  moveMotor(motorPinL1, motorPinL2, motorLEna, 0, 0);  // Stop left motor (set direction to stop and speed to 0)
 }
 
 
 /*
 Rotates servo based on position and command pressed.
 */
-void rotateServo(int servo, int direction, int endPos, int command) {
+void rotateServo(Servo &servo, int endPos) {
+  endPos = constrain(endPos, 0, 180);
+
   int pos = servo.read();
-  // RIGHT SERVO (direction = 1)
-  if (direction == 1) {
-    if (command == 'u') {
-      for (; pos <= endPos; pos++) {
+
+  if (pos < endPos) {
+    for (; pos <= endPos; pos++) {
         servo.write(pos);
         delay(15);
       }
-    } 
-    else if (command == 'd') {
-      for (; pos >= baseAngle; pos--) {
-        servo.write(pos);
-        delay(15);
-      }
-    }
   }
 
-  // LEFT SERVO (direction = -1)
-  else if (direction == -1) {
-    if (command == 'u') {
-      for (; pos >= endPos; pos--) {
+  else if (pos > endPos) {
+    for (; pos >= endPos; pos--) {
         servo.write(pos);
         delay(15);
       }
-    } 
-    else if (command == 'd') {
-      for (; pos <= baseAngle; pos++) {
-        servo.write(pos);
-        delay(15);
-      }
-    }
   }
+
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  // Commands for Controlling EnviroBot With Bluetooth Device
+  if (SerialBT.available()) {
+
+    char command = SerialBT.read();
+
+    switch(command) {
+      case 'F':
+        moveForward(speed);
+        break;
+      case 'B':
+        moveBackward(speed);
+        break;
+      case 'L':
+        turnLeft(speed);
+        break;
+      case 'R':
+        turnRight(speed);
+        break;
+      case 'S':
+        stopMotors();
+        break;
+
+      case 'X': {
+        int angle = SerialBT.parseInt();
+        rotateServo(servo1, angle);
+        break;
+      }
+
+      case 'Y': {
+        int angle = SerialBT.parseInt();
+        rotateServo(servo2, angle);
+        break;
+      }
+    }
+  }
 
 }
